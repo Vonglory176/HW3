4fa93e5f8c6b62a14cd2f1c75caad9947a47c811
Vonglory176 <SkylerGConley@gmail.com>
2023-01-19 18:19:17 -0800

    Initial push, just needs some potential cleanup

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
+    //Populating Input-Box Values
+    for (let i = 0; i < origArray.length; i++){
+        document.getElementById(i+1).value = origArray[i]
+    }
 
-        document.getElementById("textbox1").value = "success";
-    });
 });
 
+let origArray = [2,3,4]
+
+//FUNCTION 1 (Onclick)
+function square() {
+    
+    let newArray = origArray.map(i => {return (Math.pow(i, 2))}) //x.map(function(item, index, array){});
+    for (i = 0; i < newArray.length; i++){
+        document.getElementById(i+1).value = newArray[i]
+    }
+}
+
+//FUNCTION 2 (Anonymous)
+document.getElementById("bCube").addEventListener("click", function() {
+    
+    let newArray = origArray.map(i => {return (Math.pow(i, 3))}) 
+    for (i = 0; i < newArray.length; i++){
+        document.getElementById(i+1).value = newArray[i]
+    }
+})
+
+//FUNCTION 3 (Calling by name)
+document.getElementById("bFourth").addEventListener("click", b3Function)
+function b3Function() {
+
+    let newArray = origArray.map(i => {return (Math.pow(i, 4))}) 
+    for (i = 0; i < newArray.length; i++){
+        document.getElementById(i+1).value = newArray[i]
+    }
+}
