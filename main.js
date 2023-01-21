9d21bc50341f427d5fb5383930fae6c35b116b04
Vonglory176 <SkylerGConley@gmail.com>
2023-01-20 16:05:26 -0800

    Cleaned up the code a bit, outsourced calc to fourth function

--- main.js
+++ main.js
@@ -8,31 +8,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 let origArray = [2,3,4]
+let powerOf = function(j){ //Function merging previously copypasted code. 
+    //Returns (each element in origArray to the power of the inserted #) to its designated Input
 
-//FUNCTION 1 (Onclick)
-function square() {
-    
-    let newArray = origArray.map(i => {return (Math.pow(i, 2))}) //x.map(function(item, index, array){});
+    let newArray = origArray.map(i => {return (Math.pow(i, j))}) //x.map(function(item, index, array){});
     for (i = 0; i < newArray.length; i++){
         document.getElementById(i+1).value = newArray[i]
     }
 }
 
+//FUNCTION 1 (Onclick)
+function square() {powerOf(2)}
+
 //FUNCTION 2 (Anonymous)
-document.getElementById("bCube").addEventListener("click", function() {
-    
-    let newArray = origArray.map(i => {return (Math.pow(i, 3))}) 
-    for (i = 0; i < newArray.length; i++){
-        document.getElementById(i+1).value = newArray[i]
-    }
-})
+document.getElementById("bCube").addEventListener("click", function() {powerOf(3)})
 
 //FUNCTION 3 (Calling by name)
 document.getElementById("bFourth").addEventListener("click", b3Function)
-function b3Function() {
-
-    let newArray = origArray.map(i => {return (Math.pow(i, 4))}) 
-    for (i = 0; i < newArray.length; i++){
-        document.getElementById(i+1).value = newArray[i]
-    }
-}
+function b3Function() {powerOf(4)}
